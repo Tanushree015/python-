053bf90fbe4a37ac9929c38524e1d7cb2e448b24
Tanushree Shahapurmath <tanushahapurmath@gmail.com>
2026-06-09 06:32:23 +0000

    python programs

---
+++ app.py
@@ -0,0 +1,22 @@
+print("Hello,World")
+
+
+#operators
+
+# a=10
+# a +=10
+# print(a)
+# a-=10
+# print(a)
+
+
+# a=10
+# b=20
+
+# print(a<b)
+
+
+s="tanushree"
+s2="tanu"
+print(("a"  not in s))
+
