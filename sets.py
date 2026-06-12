19ebd2bb1b709ecbe967d5a23c083cdf275ddee9
Tanushree Shahapurmath <tanushahapurmath@gmail.com>
2026-06-12 06:07:45 +0000

    done

--- sets.py
+++ sets.py
@@ -26,10 +26,33 @@ print(items[-1])
 print(len(items))
 print("mouse" in items)'''
 
-fruits = {"Apple", "Banana", "Mango", "Orange"}
+'''fruits = {"Apple", "Banana", "Mango", "Orange"}
 
 print(len(fruits)   )
 
 print("Mango" in fruits)
 fruits.add("grapes")
-print(fruits)
+print(fruits)'''
+
+'''roll_numbers = {101, 102, 103, 104}
+
+print(103 in roll_numbers   )
+roll_numbers.remove(102)
+print(roll_numbers)
+print(len(roll_numbers)   ) '''
+
+
+'''movies=("bahubali","rrr","kgf","pushpa")
+print(movies[2:4])
+print("kgf" in movies)
+print(len(movies))'''
+
+
+
+products=("laptop","mouse","keyboard","mouse")
+print(products)
+print(len(products))
+print(set(products) )
+print(products)
+
+print(len(set(products)))
