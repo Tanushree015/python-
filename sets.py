0e9e1ad50e1795cc895dfc1e58d84fd6ea701d1c
Tanushree Shahapurmath <tanushahapurmath@gmail.com>
2026-06-12 05:55:50 +0000

    sets programs

---
+++ sets.py
@@ -0,0 +1,35 @@
+# sets
+
+'''a={1,2,3}
+b={2,3,4}
+u=a | b
+print(u)
+
+u =a&b
+print(u)
+
+u=a-b
+print(u)
+
+s= list([1,2,3,4])
+print(s)
+
+
+s1={1,2,3,4}
+s2={3,4,5,6}
+print(s1 | s2   )'''
+
+
+'''items=("laptop","mouse","keyboard   ")
+print(items[0])
+print(items[-1])
+print(len(items))
+print("mouse" in items)'''
+
+fruits = {"Apple", "Banana", "Mango", "Orange"}
+
+print(len(fruits)   )
+
+print("Mango" in fruits)
+fruits.add("grapes")
+print(fruits)
