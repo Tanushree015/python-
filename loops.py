b23f71f656a3deadb7ddc496881741bb9917f125
Tanushree Shahapurmath <tanushahapurmath@gmail.com>
2026-06-15 07:25:14 +0000

    done

---
+++ loops.py
@@ -0,0 +1,14 @@
+# loops
+
+'''ticket = 16
+if ticket >= 18:
+    print("allowed")
+else:
+    print("denied")'''
+
+amount = int(input("enter the amount: "))
+if amount >= 1000:
+    print("discount is applies")
+else:
+    print("no discount")
+
