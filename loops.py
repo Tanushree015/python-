6996bed5bab14aa705e1c3cc3b7150a801b65589
Tanushree Shahapurmath <tanushahapurmath@gmail.com>
2026-06-15 09:54:07 +0000

    done

--- loops.py
+++ loops.py
@@ -6,9 +6,60 @@ if ticket >= 18:
 else:
     print("denied")'''
 
-amount = int(input("enter the amount: "))
+'''amount = int(input("enter the amount: "))
 if amount >= 1000:
     print("discount is applies")
 else:
-    print("no discount")
+    print("no discount")'''
+
+
+'''marks=int(input("enter the marks: "))
+if marks>=90:
+    print("grade A")
+elif marks>=80 and marks<=90:
+    print("grade B")
+elif marks>=50 and marks<=80:
+    print("grade C")
+else:
+    print("fail")'''
+
+
+'''balance =int(input("enter the balance:"))
+if balance >= 5000:
+    print("withdrawal successfull")
+elif balance >=1000 and balance <=4999:
+    print("insufficient balance for large withdrawal")
+
+
+elif balance >=1000:
+    print("low balance")
+else:
+    print("no balance")'''
+
+
+'''signal=input("enter the signal: ")
+
+if signal== "red":
+    print("stop")
+elif signal== "yellow":
+    print("ready")
+elif signal== "green":
+    print("go")
+else:
+    print("invalid signal")'''
+
+
+'''username ="user"
+
+if username=="admin":
+    print("login successfull")
+else:
+    print("login failed")'''
+
+
+
+
+
+
+
 
